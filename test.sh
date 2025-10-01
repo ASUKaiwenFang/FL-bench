9b3050d3aef2bc65c2e97c80c11ce700c85eb280
ASUKaiwenFang <kfang11@asu.edu>
2025-10-01 19:15:27 +0000

    pin version

--- test.sh
+++ test.sh
@@ -1,10 +1,15 @@
 source .venv/bin/activate
 
-source .venv/bin/activate && python demo/main_dp_scaffold_step_noise.py optimizer.lr=0.01 model.name=2nn
-source .venv/bin/activate && python demo/main_dp_scaffold_step_noise.py optimizer.lr=0.005 model.name=2nn
+
 source .venv/bin/activate && python demo/main_dp_scaffold_step_noise.py optimizer.lr=0.001 model.name=2nn
 source .venv/bin/activate && python demo/main_dp_scaffold_step_noise.py optimizer.lr=0.0001 model.name=2nn
 
-source .venv/bin/activate && python demo/main_dp_scaffstein_step_noise_step_jse.py optimizer.lr=0.5
+
+source .venv/bin/activate && python demo/main_dp_scaffold_step_noise.py optimizer.lr=0.001 model.name=nn1
+source .venv/bin/activate && python demo/main_dp_scaffold_step_noise.py optimizer.lr=0.0001 model.name=nn1
+
+source .venv/bin/activate && python demo/main_dp_scaffstein_step_noise_step_jse.py optimizer.lr=0.5 model.name=nn1
+source .venv/bin/activate && python demo/main_dp_scaffstein_step_noise_step_jse.py optimizer.lr=0.5 model.name=2nn
+
 wait
 echo "All experiments completed!"
